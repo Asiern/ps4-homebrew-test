65654de6bc934f17764ba6e6086eca23954a4d30
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2021-12-17 20:19:19 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <sstream>
+#include <iostream>
+#include <orbis/libkernel.h>
+
+#include "../include/log.h"
+
+// Logging
+std::stringstream debugLogStream;
+
+int main(void)
+{
+    int sleepSeconds = 2;
+    
+    // No buffering
+    setvbuf(stdout, NULL, _IONBF, 0);
+    
+    DEBUGLOG << "Hello world! Waiting " << sleepSeconds << " seconds!";
+    sceKernelUsleep(2 * 1000000);
+    DEBUGLOG << "Done. Infinitely looping...";
+
+    for (;;) {}
+
+    return 0;
+}
